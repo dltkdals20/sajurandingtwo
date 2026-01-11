c398321933994134825695a4ce72ee4f0f144811
eunji <eunji@eunjiui-Macmini.local>
2026-01-11 17:58:53 +0900

    반응형 레이아웃 수정: 모바일에서도 웹과 동일한 디자인 유지

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,14 +3,16 @@ import MobileLanding from "./MobileLanding";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-[#faf8f5]">
+    <div>
       <div className="md:hidden">
         <MobileLanding />
       </div>
       <div className="hidden md:block">
-        <div className="h-screen overflow-auto">
-          <div className="mx-auto w-[888px]">
-            <Group9 />
+        <div className="min-h-screen bg-[#faf8f5]">
+          <div className="h-screen overflow-auto">
+            <div className="mx-auto w-[888px]">
+              <Group9 />
+            </div>
           </div>
         </div>
       </div>
